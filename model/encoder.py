5e2483c13275db412bf61d0a5efa73b8ef041de8
stravsm@euler <stravs@imsb.biol.ethz.ch>
2024-06-03 14:29:27 +0200

    fix dropout in encoder

--- model/encoder.py
+++ model/encoder.py
@@ -19,6 +19,7 @@ class FingerprintFormulaEncoder(tf.keras.layers.Layer):
                  zero_out = False,
                  input_dropout = None,
                  dropout = None,
+                 training = True,
                  **kwargs):
         
         super().__init__(name = name, **kwargs)        
@@ -26,15 +27,24 @@ class FingerprintFormulaEncoder(tf.keras.layers.Layer):
         self.name_ = name
         self.zero_out = zero_out
         self.dropout_ = dropout
+        self.input_dropout_ = input_dropout
+        self.training_ = training
+
+        if self.dropout_ is not None:
+            if len(self.layers_) != len(self.dropout_):
+                raise ValueError("Number of layers is not equal to number of dropout coefficients")
         
         self.zeroable = ZeroableLayer(zero_out = self.zero_out)
         
         self.batchnorm = BatchNormalization()
         self.layers = {}
         self.dropout = {}
+
+        self.input_dropout = None
+
         for i, units in enumerate(self.layers_):
             self.layers[i] = Dense(units, name=f'{self.name_}_enc_{i}')
-            if self.dropout is not None:
+            if self.dropout_ is not None:
                 self.dropout[i] = Dropout(self.dropout_[i])
             else:
                 self.dropout[i] = None
@@ -45,6 +55,8 @@ class FingerprintFormulaEncoder(tf.keras.layers.Layer):
                  ]
                 for i in range(layers_decoder)
             ]
+        if self.input_dropout_ is not None:
+            self.input_dropout = Dropout(self.input_dropout_)
         
     def call(self, inputs):
         '''
@@ -63,10 +75,12 @@ class FingerprintFormulaEncoder(tf.keras.layers.Layer):
         
         layer_stack = concatenate(inputs)
         layer_stack = self.batchnorm(layer_stack)
+        # if self.input_dropout is not None:
+        #     layer_stack = self.input_dropout(layer_stack, training = self.training_)
         for layer, dropout in zip(self.layers.values(), self.dropout.values()):
             layer_stack = layer(layer_stack)
             if dropout is not None:
-                layer_stack = dropout(layer_stack, training = True)
+                layer_stack = dropout(layer_stack, training = self.training_)
         z = layer_stack
         z_ = self.zeroable(z)
         # Transform the z to i*j starting states for the RNN
